8f16e2ea8ec2bb78c4ab8a4fa0e26cdacb533e3f
Tan Feng <wstanfeng@gmail.com>
2024-10-19 10:29:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -68,7 +68,7 @@ git_sparse_clone main https://github.com/linkease/istore luci
 
 #修复freeswitch依赖缺失
 PKG_PATCH="$GITHUB_WORKSPACE/openwrt/package/"
-FW_FILE=$(find ../feeds/telephony/ -maxdepth 4 -type f -wholename "*/freeswitch/Makefile")
+FW_FILE=$(find package/feeds/telephony/ -maxdepth 4 -type f -wholename "*/freeswitch/Makefile")
 if [ -f "$FW_FILE" ]; then
 	sed -i "s/libpcre/libpcre2/g" $FW_FILE
 
