c28878d9f7043b504cebbdc3fde5da2a3bec0efc
Tan Feng <wstanfeng@gmail.com>
2024-10-22 14:42:02 +0800

    Update diy_lede.sh

--- diy_lede.sh
+++ diy_lede.sh
@@ -43,8 +43,8 @@ rm -rf feeds/luci/applications/luci-app-ddns-go
 rm -rf feeds/packages/net/v2ray-geodata
 
 #golang 1.22.x or latest version
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 # mosdns
 git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
