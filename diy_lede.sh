4024e0481ddd43488e2a5259f0dbea0c07f3252e
Tan Feng <wstanfeng@gmail.com>
2024-10-24 19:29:42 +0800

    Update diy_lede.sh

--- diy_lede.sh
+++ diy_lede.sh
@@ -60,7 +60,6 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 
 #adguard-core.sh
 mkdir -p files/usr/bin/AdGuardHome
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
+AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_amd64 | awk -F '"' '{print $4}')
 wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
 chmod +x files/usr/bin/AdGuardHome/AdGuardHome
-
